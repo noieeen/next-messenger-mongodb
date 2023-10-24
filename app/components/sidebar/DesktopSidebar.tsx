a95998f0544b4eb69f3b2938f55eb45b7df10a40
Tharin <tharin.ch@chococrm.com>
2023-10-25 02:11:04 +0700

    fixed setting open

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -19,7 +19,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
     <>
       <SettingModal
         currentUser={currentUser}
-        isOpen={true}
+        isOpen={isOpen}
         onClose={() => setIsOpen(false)}
       />
       <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
