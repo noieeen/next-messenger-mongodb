2430137d99593584814104d3df52c6ccf016aad9
Tharin <tharin.ch@chococrm.com>
2023-11-21 02:55:43 +0700

    add check active status [final]

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 import ToasterContext from "./context/ToastContext";
 import AuthContext from "./context/AuthContext";
+import ActiveStatus from "./components/ActiveStatus";
 
 export const metadata: Metadata = {
   title: "Messenger",
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthContext>
+          <ActiveStatus />
           <ToasterContext />
           {children}
         </AuthContext>
